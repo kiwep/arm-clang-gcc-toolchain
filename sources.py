d0f955b68f37fd102257efc13b848ff6433d14de
Kovács Péter <peter@kwep.me>
2017-07-18 13:21:10 +0200

    Basic python builder script: downloading and llvm cmake config

---
+++ sources.py
@@ -0,0 +1,10 @@
+svn = {
+    "llvm": "http://llvm.org/svn/llvm-project/llvm/trunk",
+    "clang": "http://llvm.org/svn/llvm-project/cfe/trunk"
+}
+
+arm = {
+    "base": "https://developer.arm.com",
+    "dlpage": "/open-source/gnu-toolchain/gnu-rm/downloads",
+    "link": "/-/media/Files/downloads/gnu-rm/"
+}
